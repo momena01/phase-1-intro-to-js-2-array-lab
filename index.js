bb1f80ac0247891835a7ac2bcb99d43b2f434e89
momena <momena.salloum59@gmail.com>
2023-09-29 21:18:14 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+
+function destructivelyAppendCat(Ralph) {
+cats.push(Ralph)
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(Broom){
+    const copyCats = cats.slice()
+    copyCats.push(Broom)
+    return copyCats; 
+}
+function prependCat(Arnold) {
+    const copyCats = cats.slice()
+    copyCats.unshift(Arnold);
+    return copyCats;
+}
+function removeFirstCat() {
+    const copyCats = cats.slice()
+    copyCats.shift();
+    return copyCats;
+}
+function removeLastCat() {
+    const copyCats = cats.slice()
+    copyCats.pop();
+    return copyCats;
+}
+console.log(cats)
